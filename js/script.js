e33c046e8650f8234cd934e28366a7b7cb556c7d
zolamanv <31780967+zolamanv@users.noreply.github.com>
2017-10-14 11:58:04 +0100

    4482 ver01

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).on('click', 'a[href="#"]', function(e) {
+    e.preventDefault();
+});
+
+$('#exhibitions-years .slide-image ul.indicator li a').on('click', function(e) {
+  var index = $('ul.indicator li').index($(this).parent());
+  $('#exhibitions-years .slide-image ul.image li').css({'display': 'none'});
+  $('#exhibitions-years .slide-image ul.image li:eq(' + index + ')').css({'display': 'block'});
+  $('#exhibitions-years .slide-image ul.indicator li').removeClass('on');
+  //$('#main-visual ul.indicator li:eq(' + index + ').addClass('on');
+  $(this).parent().addClass('on');
+  e.preventDefault();
+});
