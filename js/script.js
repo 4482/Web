b7cee34cd09a09d613e8de8c5d352cd9622616cc
zolamanv <31780967+zolamanv@users.noreply.github.com>
2018-04-30 18:46:42 +0100

    final

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(document).on('click', 'a[href="#"]', function(e) {
     e.preventDefault();
 });
 
-$('#exhibitions-years .slide-image ul.indicator li a').on('click', function(e) {
+$('.slide-image ul.indicator li a').on('click', function(e) {
   var index = $('ul.indicator li').index($(this).parent());
-  $('#exhibitions-years .slide-image ul.image li').css({'display': 'none'});
-  $('#exhibitions-years .slide-image ul.image li:eq(' + index + ')').css({'display': 'block'});
-  $('#exhibitions-years .slide-image ul.indicator li').removeClass('on');
+  $('.slide-image ul.image li').css({'display': 'none'});
+  $('.slide-image ul.image li:eq(' + index + ')').css({'display': 'block'});
+  $('.slide-image ul.indicator li').removeClass('on');
   //$('#main-visual ul.indicator li:eq(' + index + ').addClass('on');
   $(this).parent().addClass('on');
   e.preventDefault();
